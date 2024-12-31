2c383b977ce9366c498691a23330c5bc7d0f45c8
niketsuchak <niket.suchak2021@vitstudent.ac.in>
2024-12-31 11:08:52 +0530

    intializing repo

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function layout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
